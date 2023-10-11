fd14f6ae5adaf75b6152d2c11e42d18e956e64a6
AbdulKudus Omotosho <40666393+Abdulkudusomot@users.noreply.github.com>
2023-10-11 23:10:25 +0100

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,118 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+import product10 from "./assets/products/10.JPG";
+import product11 from "./assets/products/11.PNG";
+import product12 from "./assets/products/12.JPG";
+import product13 from "./assets/products/13.PNG";
+import product14 from "./assets/products/14.jpg";
+import product15 from "./assets/products/15.PNG";
+import product16 from "./assets/products/16.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 80000,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 150000,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Quality",
+    price: 30000,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "WLS Van Gogh Denim Jacket",
+    price: 15000,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "High Heel(Niger_Made)",
+    price: 10000,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SPECTRUM LS TEE",
+    price: 10000,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    price: 20000,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    price: 5000,
+    productImage: product8,
+  },
+  {
+    id: 10,
+    productName: "Designer",
+    price: 20000,
+    productImage: product10,
+  },
+
+{
+    id: 11,
+    productName: "Niger Made",
+    price: 10000,
+    productImage: product11,
+  },
+
+  {
+    id: 12,
+    productName: "King flat slippers",
+    price: 20000,
+    productImage: product12,
+  },
+
+  {
+    id: 13,
+    productName: "Hand-made slippers",
+    price: 4000,
+    productImage: product13,
+  },
+
+  {
+    id: 14,
+    productName: "Kube Carp",
+    price: 20000,
+    productImage: product14,
+  },
+
+  {
+    id: 15,
+    productName: "Niger_Made",
+    price: 4000,
+    productImage: product15,
+  },
+
+  {
+    id: 16,
+    productName: "Kube_Guys",
+    price: 20000,
+    productImage: product16,
+  },
+
+  
+];
+
+
